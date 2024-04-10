40de73554d7b45447092ff5bf8add8dc2ee2809a
yammyangle <109473833+yammyangle@users.noreply.github.com>
2024-04-10 18:28:23 +0100

    Add files via upload

---
+++ AE3/README.md
@@ -0,0 +1,7 @@
+# uk.ac.nulondon:AE3
+
+Write your project description here...
+
+Java version 21
+
+Generated at 2024-04-10 18:11:42
---
+++ AE3/src/main/java/uk/ac/nulondon/App.java
@@ -0,0 +1,10 @@
+package uk.ac.nulondon;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
